34bea2d05d278e8cc640f81a44334d91eca5a72c
Samuel <sotto@htl-steyr.ac.at>
2021-01-20 12:28:07 +0100

    Sniping completed

--- Button.py
+++ Button.py
@@ -7,17 +7,13 @@ class Button:
         self.y = y
 
     def __init__(self, x, y, w, h):
-        self.x = x + w
-        self.y = y + h / 2
+        self.x = int(x + w)
+        self.y = int(y + h / 2)
         self.active = False
         self.onCooldown = False
 
     def click(self):
         pyautogui.click(x=self.x, y=self.y)
-
-    # def active(self):
-    #     pix = pyautogui.pixel(self.x, self.y)
-    #     return pyautogui.pixelMatchesColor(, (124, 78, 78))
 
     def is_on_cooldown(self):
         pix = pyautogui.pixel(self.x, self.y)
--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 import pyautogui
 import cv2 as cv
 import numpy as np
-from Button import Button
 import time
+from Button import Button
 
 print(pyautogui.size())
-
-pimg = pyautogui.screenshot()
-print(pimg)
-
-open_cv_image = np.array(pimg)
-
-# Convert RGB to BGR
-open_cv_image = open_cv_image[:, :, ::-1].copy()
 
 temp1 = list(pyautogui.locateAllOnScreen('res/buOffensive.PNG'))
 temp2 = list(pyautogui.locateAllOnScreen('res/buDefensive.PNG'))
@@ -37,10 +29,14 @@ for b in temp3:
 
 offensivButtons[1].click()
 
+priorityOrder = [defensivButtons[5], defensivButtons[3], defensivButtons[4], offensivButtons[5], offensivButtons[4],
+                 offensivButtons[2], miscButtons[0], defensivButtons[2], miscButtons[1], offensivButtons[1]]
+
 while True:
     time.sleep(0.02)
-    if not offensivButtons[1].is_on_cooldown():
-        offensivButtons[1].click()
-
+    for bu in priorityOrder:
+        if not bu.is_on_cooldown():
+            bu.click()
+            break
 
 print("end")
